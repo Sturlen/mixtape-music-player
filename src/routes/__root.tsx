7e2f312b74b3734db0f5462fa06dde8da0443a4e
Sture Spetland <43607012+Sturlen@users.noreply.github.com>
2025-10-24 21:26:30 +0200

    refactor: restructure RootLayout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,55 +1,69 @@
 import { ScrollArea } from "@/Components/ui/scroll-area"
 import Controls from "@/Controls"
+import { cn } from "@/lib/utils"
 import { MediaSessionSync } from "@/MediaSessionSync"
 import PlaybackQueue from "@/QueueList"
 import VolumeSlider from "@/VolumeControl"
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
+import { UserIcon } from "lucide-react"
 
 const RootLayout = () => (
-  <div className="h-full grid grid-rows-[8rem_1fr]">
-    <header className="bg-black h-full flex flex-col">
-      <div className="p-4 grow">
-        <h1 className="text-3xl italic font-serif font-battle">MIXTAPE ZX25</h1>
-      </div>
-      <div className="flex">
-        <Link
-          to="/"
-          className="[&.active]:font-bold [&.active]:bg-secondary p-4"
-        >
+  <div className="h-full bg-gradient-to-b from-accent from-[200px] to-black to-[600px]">
+    <header className="bg-black h-30 flex p-2 md:p-10 items-center justify-between fixed top-0 left-0 xl:left-[25rem] right-0">
+      <h1 className="text-2xl md:text-3xl italic font-serif font-battle whitespace-nowrap">
+        MIXTAPE
+      </h1>
+      {/* <div className="grow"></div> */}
+      <nav className="md:flex justify-around items-center [&>*]:p-4 w-full hidden">
+        <Link to="/" className="[&.active]:font-bold [&.active]:bg-secondary">
           Home
         </Link>
         <Link
           to="/about"
-          className="[&.active]:font-bold [&.active]:bg-secondary  p-4"
+          className="[&.active]:font-bold [&.active]:bg-secondary"
         >
           About
         </Link>
+      </nav>
+
+      <div title="WIP">
+        <UserIcon />
       </div>
     </header>
-    <div className="grid lg:grid-cols-[20rem_1fr] xl:grid-cols-[32rem_1fr] h-full overflow-hidden gap-4">
-      <section className="lg:flex flex-col p-10 border-4 hidden items-center">
-        <h2 className="text-2xl text-center pb-2 font-inter-400">QUEUE</h2>
-        <PlaybackQueue />
-        <div className="grow"></div>
-        <h2 className="text-2xl text-center pb-2 font-inter-400">
-          PLAYBACK CONTROLS
-        </h2>
-        <div className="bg-accent rounded-2xl w-full p-4 flex-col items-center justify-items-center">
-          <div className="flex justify-items-center items-center">
-            <Controls />
-            <VolumeSlider />
-          </div>
-        </div>
-      </section>
-      <ScrollArea className="overflow-y-auto bg-gradient-to-b from-accent from-20% to-black">
+    <Sidebar className="w-[25rem] hidden xl:flex" />
+    <div className="pt-30 xl:pl-[25rem]">
+      <div>
         <Outlet />
-      </ScrollArea>
+      </div>
     </div>
 
-    <TanStackRouterDevtools />
+    <TanStackRouterDevtools position="top-right" />
     <MediaSessionSync />
   </div>
 )
 
+function Sidebar({ className }: { className?: string }) {
+  return (
+    <section
+      className={cn(
+        "fixed top-0 bottom-0 left-0 flex flex-col p-10 border-4 items-center bg-background",
+        className
+      )}
+    >
+      <h2 className="text-2xl text-center pb-2 font-inter-400">QUEUE</h2>
+      <PlaybackQueue />
+      <div className="grow"></div>
+      <h2 className="text-2xl text-center pb-2 font-inter-400">
+        PLAYBACK CONTROLS
+      </h2>
+      <div className="self-end bg-accent rounded-2xl w-full p-4 flex-col items-center justify-items-center">
+        <div className="flex justify-items-center items-center">
+          <Controls />
+          <VolumeSlider />
+        </div>
+      </div>
+    </section>
+  )
+}
 export const Route = createRootRoute({ component: RootLayout })
