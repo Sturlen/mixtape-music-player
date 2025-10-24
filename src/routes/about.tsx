73fdfc24fd11edb35ad03c20c0a00d3c9d00f359
Sture Spetland <43607012+Sturlen@users.noreply.github.com>
2025-10-24 20:33:57 +0200

    chore: cassette image credits

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -10,6 +10,14 @@ function About() {
     <Page>
       <div style={{ fontStyle: "italic" }}>Brudeferden i Hardanger</div>
       <div>Photo: Nasjonalmuseet / Børre Høstland</div>
+      Cassette Photo by <hr />
+      <a href="https://unsplash.com/@etiennegirardet?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Etienne Girardet
+      </a>{" "}
+      on{" "}
+      <a href="https://unsplash.com/photos/a-black-and-white-tape-recorder-on-a-white-background-WWVnwP4D-N0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Unsplash
+      </a>
     </Page>
   )
 }
