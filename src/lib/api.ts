42ee1ebb1e37ebc507791d80d3ed62c4000b61cb
Sture Spetland <43607012+Sturlen@users.noreply.github.com>
2025-11-15 17:33:43 +0100

    load tr no

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -44,6 +44,7 @@ export function usePlayAlbum() {
             id: album.id,
             name: album.name,
           },
+          trackNumber: track.trackNumber,
         })),
         trackIndex,
       )
